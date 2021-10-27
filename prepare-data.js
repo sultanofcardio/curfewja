d5f7519d0cb5b45646214f1c54d027eadb2a02af
sultanofcardio <kenroy.gobourne@gmail.com>
2021-10-26 19:41:54 -0500

    Added an info icon with the last updated date

--- prepare-data.js
+++ prepare-data.js
@@ -1,4 +1,6 @@
 import {curfews} from "./src/model/data.js"
+import fetch from 'node-fetch'
+import momentTz from "moment-timezone";
 import * as fs from 'fs'
 
 // Write the curfew data to a JSON file to make the data publicly available
@@ -8,3 +10,13 @@ const data = curfews.map(it => ({
 })).reverse()
 
 fs.writeFileSync('public/data.json', JSON.stringify(data))
+
+// Write the last updated time
+const URL = `https://api.github.com/repos/sultanofcardio/curfewja/commits?path=${encodeURIComponent('src/model/data.js')}&page=1&per_page=1`
+fetch(URL)
+  .then(response => response.json())
+  .then(json => {
+    const lastUpdatedDate = momentTz.tz(json[0].commit.committer.date, 'America/Jamaica')
+    fs.writeFileSync('public/last-updated.txt', lastUpdatedDate.format())
+  })
+  .catch(console.error)
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,28 @@
 import './Home.css';
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react"
 import {Countdown} from "./Countdown";
 import {Context} from "../../util/context";
 import {Link} from "react-router-dom";
 import {GitHubIcon} from "../GitHubIcon";
+import ReactTooltip from 'react-tooltip'
+import momentTz from 'moment-timezone'
+import infoIcon from '../../img/info-circle.svg'
+import {TIME_ZONE} from '../../model/Curfew'
 
 function Home() {
 
+  const [lastUpdatedDate, setLastUpdated] = useState(null)
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await window.fetch('/last-updated.txt')
+        const dateString = await response.text()
+        setLastUpdated(dateString)
+      } catch (e){}
+    }
+    getData()
+  }, [])
 
   const {
     state: {
@@ -17,12 +33,19 @@ function Home() {
     }
   } = useContext(Context)
 
+  const verbiage = lastUpdatedDate ? `Last updated ${momentTz.tz(TIME_ZONE).to(lastUpdatedDate)}` : 'Last updated info unavailable'
+
   return (
     <div className={`App ${curfewData.status.class}`}>
       <span className="curfew-status">{curfewData.status.title}</span>
       <Countdown />
       <span className="curfew-end-time">{curfewData.detail}</span>
       <Link to="/schedule" className="curfew-schedule-btn">View schedule</Link>
+      <img className='curfew-info-icon'
+           src={infoIcon}
+           data-tip={verbiage}
+           alt={verbiage} />
+      <ReactTooltip />
       <GitHubIcon/>
     </div>
   );
